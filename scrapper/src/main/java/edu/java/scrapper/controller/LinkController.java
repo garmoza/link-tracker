ef4040d5c6cac46c5c05b726b8800b885dee0a70
Nikita Garmoza <garmozanikita@gmail.com>
2024-03-03 20:39:38 +0300

    Contract-first `LinkController`, `TgChatController`

--- scrapper/src/main/java/edu/java/scrapper/controller/LinkController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/LinkController.java
@@ -4,45 +4,32 @@ import edu.java.model.request.AddLinkRequest;
 import edu.java.model.request.RemoveLinkRequest;
 import edu.java.model.response.LinkResponse;
 import edu.java.model.response.ListLinksResponse;
+import edu.java.scrapper.api.LinkControllerApi;
 import edu.java.scrapper.service.LinkService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/links")
 @RequiredArgsConstructor
-@Validated
-public class LinkController {
+public class LinkController implements LinkControllerApi {
 
     private final LinkService linkService;
 
-    @GetMapping
-    public ResponseEntity<ListLinksResponse> getAllLinks(@Positive @RequestHeader("Tg-Chat-Id") long tgChatId) {
+    @Override
+    public ResponseEntity<ListLinksResponse> getAllLinks(Long tgChatId) {
         return linkService.getAllLinks(tgChatId);
     }
 
-    @PostMapping
-    public ResponseEntity<LinkResponse> addLink(
-        @Positive @RequestHeader("Tg-Chat-Id") long tgChatId,
-        @Valid @RequestBody AddLinkRequest dto
-    ) {
+    @Override
+    public ResponseEntity<LinkResponse> addLink(Long tgChatId, AddLinkRequest dto) {
         return linkService.addLink(tgChatId, dto);
     }
 
-    @DeleteMapping
+    @Override
     public ResponseEntity<LinkResponse> deleteLink(
-        @Positive @RequestHeader("Tg-Chat-Id") long tgChatId,
-        @Valid @RequestBody RemoveLinkRequest dto
+        Long tgChatId,
+        RemoveLinkRequest dto
     ) {
         return linkService.deleteLink(tgChatId, dto);
     }
--- scrapper/src/main/java/edu/java/scrapper/controller/TgChatController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/TgChatController.java
@@ -1,31 +1,24 @@
 package edu.java.scrapper.controller;
 
+import edu.java.scrapper.api.TgChatControllerApi;
 import edu.java.scrapper.service.TgChatService;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tg-chat")
 @RequiredArgsConstructor
-@Validated
-public class TgChatController {
+public class TgChatController implements TgChatControllerApi {
 
     private final TgChatService tgChatService;
 
-    @PostMapping("/{id}")
-    public ResponseEntity<Void> registerChat(@Positive @PathVariable long id) {
+    @Override
+    public ResponseEntity<Void> registerChat(Long id) {
         return tgChatService.registerChat(id);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteChat(@Positive @PathVariable long id) {
+    @Override
+    public ResponseEntity<Void> deleteChat(Long id) {
         return tgChatService.deleteChat(id);
     }
 }
