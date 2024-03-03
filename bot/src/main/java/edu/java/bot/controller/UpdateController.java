4d4c444a666aab3053ee8bf12613e1d6601c1a17
Nikita Garmoza <garmozanikita@gmail.com>
2024-03-03 19:45:54 +0300

    Contract-first `UpdateController`

--- bot/src/main/java/edu/java/bot/controller/UpdateController.java
+++ bot/src/main/java/edu/java/bot/controller/UpdateController.java
@@ -1,26 +1,20 @@
 package edu.java.bot.controller;
 
+import edu.java.bot.api.UpdateControllerApi;
 import edu.java.bot.service.UpdateService;
 import edu.java.model.request.LinkUpdate;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/updates")
 @RequiredArgsConstructor
-@Validated
-public class UpdateController {
+public class UpdateController implements UpdateControllerApi {
 
     private final UpdateService updateService;
 
-    @PostMapping
-    public ResponseEntity<Void> sendUpdate(@Valid @RequestBody LinkUpdate dto) {
+    @Override
+    public ResponseEntity<Void> sendUpdate(LinkUpdate dto) {
         return updateService.sendUpdate(dto);
     }
 }
